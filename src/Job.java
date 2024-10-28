39c16118f8fa57d4e87d7cdc134505073b23771b
JUSTINPARDO <100895234+JUSTINPARDO@users.noreply.github.com>
2024-10-27 23:04:00 -0500

    Update Job.java

--- src/Job.java
+++ src/Job.java
@@ -1,1 +1,59 @@
+public class Job implements Comparable<Job> {
+    private int id;
+    private int processingTime;
+    private int priority;
+    private int arrivalTime;
+    private boolean hasPriority;
 
+    public Job(int id, int processingTime) {
+        this.id = id;
+        this.processingTime = processingTime;
+        this.priority = 0;
+        this.arrivalTime = 0;
+        this.hasPriority = false;
+    }
+
+    public static Job createWithPriority(int id, int processingTime, int priority) {
+        Job job = new Job(id, processingTime);
+        job.priority = priority;
+        job.hasPriority = true;
+        return job;
+    }
+
+    public static Job createWithArrivalTime(int id, int processingTime, int arrivalTime) {
+        Job job = new Job(id, processingTime);
+        job.arrivalTime = arrivalTime;
+        return job;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProcessingTime() {
+        return processingTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public boolean hasPriority() {
+        return hasPriority;
+    }
+
+    @Override
+    public int compareTo(Job other) {
+        return Integer.compare(this.processingTime, other.processingTime);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+}
